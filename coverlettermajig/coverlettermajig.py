757a9ab253bc1fdc1d27c37366abdddb3ad765f3
jason fukura <jfukura@gmail.com>
2023-01-17 18:54:15 -0800

    some updates

--- coverlettermajig/coverlettermajig.py
+++ coverlettermajig/coverlettermajig.py
@@ -1,5 +1,3 @@
-import ctypes
-
 class TermColors:
     def colorize(self, _type: str = 'info', _content: str = ''):
         _colors = {
@@ -23,18 +21,27 @@ class BuildCoverLetter:
         self._ux = False
         self._design = False
         self._tc = TermColors()
-
-        self._input_valprop = ''
+        self._custom_salutation = {
+            "question": " Do you know the name of the person you are contacting (Default: \"hiring manager\")? (y/N) ",
+            "answer": False
+        }
+        self._salutation = "hiring manager"
+        self._input_valprop = {
+            "question": " Do you want to customize the valprop? (y/N) ",
+            "answer": False
+        }
+        self._default_valprop = "With a deep understanding of the problem-solving process from start to finish"
+        self._additional_intro = False
         self._type_uxd = {
-            "question": " Is this a UX focused job?",
+            "question": " Is this a UX focused job? (y/N) ",
             "answer": False
         }
         self._type_dev = {
-            "question": " Is this a development focused job?",
+            "question": " Is this a development focused job? (y/N) ",
             "answer": False
         }
         self._type_des = {
-            "question": " Is this a design focused job?",
+            "question": " Is this a design focused job? (y/N) ",
             "answer": False
         }
 
@@ -47,7 +54,7 @@ class BuildCoverLetter:
                           "|     |    |  |  ||     ||  |  |  |  |\n  |__|  |" \
                           "__|__||_____|    |__|__| \__,_||__|__|  |__|     " \
                           "                                                   "
-        self._intro_title = "                 Cover Letter Builder           " \
+        self._intro_title = "                 Coverlettermajig               " \
                             "       "
         # Initializing prints the intro art
         print('\n')
@@ -57,25 +64,42 @@ class BuildCoverLetter:
         pass
 
     def identify_job(self):
-        # Ask for name of company
-        self._company = input(self._tc.colorize(
-            _content=" What is the name of the company?"))
-
-        # Ask for job title
-        self._title = input(self._tc.colorize(
-            _content=" What is the job title?"))
-
         def _boolean_question(option):
             while True:
                 _ans = input(self._tc.colorize(_content=option["question"]))
-                if _ans == '' \
-                        or not _ans[0].lower() in ['y', 'n']:
-                    print('Please answer with yes or no!')
-                else:
-                    break
-            if _ans[0].lower() == 'y':
-                option["answer"] = True
+                break
+
+            if _ans == '':
                 pass
+            else:
+                if not _ans[0].lower() in ['y', 'n']:
+                    print('Please answer with yes or no!')
+                elif _ans[0].lower() == 'y':
+                    option["answer"] = True
+                    pass
+
+        # Ask for name of company
+        self._company = input(self._tc.colorize(
+            _content=" What is the name of the company? "))
+
+        # Ask for job title
+        self._title = input(self._tc.colorize(
+            _content=" What is the job title? "))
+
+        _boolean_question(self._custom_salutation)
+        if self._custom_salutation["answer"]:
+            # Ask to customize the salutation
+            self._salutation = input(self._tc.colorize(
+                _content=" What's the name of the person you are contacting? "
+            ))
+
+        _boolean_question(self._input_valprop)
+
+        if self._input_valprop["answer"]:
+            # Ask to customize the intro
+            self._default_valprop = input(self._tc.colorize(
+                _content=" Change the valprop here (this will be the first part of a comma-separated clause) "
+            ))
 
         _boolean_question(self._type_uxd)
         _boolean_question(self._type_dev)
@@ -85,25 +109,27 @@ class BuildCoverLetter:
 
     def build_output(self):
         _content = []
-        with open("intro.txt", "r", encoding="utf-8") as input_file:
+        with open("../data/intro.txt", "r", encoding="utf-8") as input_file:
             _copy = input_file.read()
+            _copy = _copy.replace('{{ salutation }}', self._salutation)
             _copy = _copy.replace('{{ company }}', self._company)
             _copy = _copy.replace('{{ position }}', self._title)
+            _copy = _copy.replace('{{ additional_intro }}', self._default_valprop)
             _content.append(_copy)
 
         if self._type_uxd['answer'] is True:
-            with open("uxd.txt", "r", encoding="utf-8") as input_file:
+            with open("../data/uxd.txt", "r", encoding="utf-8") as input_file:
                 _content.append(input_file.read())
 
         if self._type_dev['answer'] is True:
-            with open("dev.txt", "r", encoding="utf-8") as input_file:
+            with open("../data/dev.txt", "r", encoding="utf-8") as input_file:
                 _content.append(input_file.read())
 
         if self._type_des['answer'] is True:
-            with open("des.txt", "r", encoding="utf-8") as input_file:
+            with open("../data/des.txt", "r", encoding="utf-8") as input_file:
                 _content.append(input_file.read())
 
-        with open("closing.txt", "r", encoding="utf-8") as input_file:
+        with open("../data/closing.txt", "r", encoding="utf-8") as input_file:
             _content.append(input_file.read())
 
         print("\n".join(_content))
@@ -112,5 +138,6 @@ class BuildCoverLetter:
         pass
 
 
-cover = BuildCoverLetter()
-cover.identify_job()
+if __name__ == "__main__":
+    cover = BuildCoverLetter()
+    cover.identify_job()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Coverlettermajig',
+    version='0.1',
+    author='Jason Fukura',
+    author_email='jason@jasonfukura.com',
+    packages=find_packages(),
+    long_description=open('README.md').read()
+)
